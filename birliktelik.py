488b94d24ad07cf6011f29d75212ef3d7424de89
Selçuk Sinan KIRAT <53806903+ogretMAN@users.noreply.github.com>
2020-04-29 08:41:04 +0300

    Update birliktelik.py

--- birliktelik.py
+++ birliktelik.py
@@ -4,36 +4,42 @@
 Copyright 2020 Selçuk Sinan KIRAT
 
 English Text
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+<Python program of data mining association rulse>
+    Copyright (C) <2020>  <Selçuk Sinan KIRAT>
 
-http://www.apache.org/licenses/LICENSE-2.0
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ---------------------------------------------------------
 
 Türkçe Metin
-Apache Lisansı, Sürüm 2.0 (işbu “Lisans”) ile lisanslanan
-bu dosyayı, işbu lisansla uyumlu olan durumlar dışında kullanamazsınız.
-Lisansın bir kopyasını        
+<Veri Madenciliği Birliktelik Kuralları Python Programı>
 
-http://www.apache.org/licenses/LICENSE-2.0
+    Copyright (C) <2020> <Selçuk Sinan KIRAT>
 
-adresinden temin edebilirsiniz.
-Yürürlükteki bir yasada belirtilmediği veya yazılı olarak
-beyan edilmediği sürece, işbu lisans altında dağıtılan
-yazılım “hiçbir değişiklik yapılmadan” esasıyla dağıtılmış
-olup, açıkça veya üstü kapalı olarak, HİÇBİR TEMİNAT VEYA
-KOŞUL İÇERMEZ. Özel dil uygulama izinleri ve işbu lisans altındaki kısıtlamalar için lisansa bakınız.
+    Bu program özgür yazılımdır: Özgür Yazılım Vakfı tarafından yayımlanan GNU Genel Kamu Lisansı’nın sürüm 3 ya da 
+    (isteğinize bağlı olarak) daha sonraki sürümlerinin hükümleri altında yeniden dağıtabilir ve/veya değiştirebilirsiniz.
 
-Selçuk Sinan KIRAT (Bilgisayar Öğretmeni)
-http://www.selcuksinankirat.com
+    Bu program, yararlı olması umuduyla dağıtılmış olup, programın BİR TEMİNATI YOKTUR; TİCARETİNİN YAPILABİLİRLİĞİNE VE 
+    ÖZEL BİR AMAÇ İÇİN UYGUNLUĞUNA dair bir teminat da vermez. Ayrıntılar için GNU Genel Kamu Lisansı’na göz atınız.
+
+    Bu programla birlikte GNU Genel Kamu Lisansı’nın bir kopyasını elde etmiş olmanız gerekir. 
+    Eğer elinize ulaşmadıysa <http://www.gnu.org/licenses/> adresine bakınız.
+    
+    Tükrçe çeviri: http://ozgurlisanslar.org.tr/gpl/gpl-v3/
+
+    Selçuk Sinan KIRAT (Bilgisayar Öğretmeni)
+    http://www.selcuksinankirat.com
 
 """
 
